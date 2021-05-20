ed79192d49e3add71ea3c5a4794e48178be83bdd
mmorrisonlk <mmorrisonlk@gmail.com>
2021-05-20 18:06:18 -0500

    modifying restaurant code to direct to html

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // require('./routes/apiRoutes')(app);
-// require('./routes/htmlRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
